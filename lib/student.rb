37aadaa168c8790646c837e5efb403db56772b87
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-08-20 01:20:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,11 +5,10 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    #@name = student_hash[:name]
     self.send("name=", student_hash[:name])
-    #@location = student_hash[:location]
+ 
     self.send("location=", student_hash[:location])
-    #@profile_url = student_hash[:profile_url]
+ 
     self.send("profile_url=", student_hash[:profile_url])
     @@all << self
   end
